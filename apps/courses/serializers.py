669a309e7645c92136828a170178e05f02f0e8ee
Toktorov <ktoktorov2005@gmail.com>
2021-09-11 10:59:31 +0600

    add students for klasses

--- apps/courses/serializers.py
+++ apps/courses/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from apps.courses.models import Course
 from apps.klasses.serializers import KlassSerializer
+from apps.students.serializers import StudentSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     klasses = KlassSerializer(many=True, read_only=True)
+    students = StudentSerializer(many = True, read_only = True)
 
     class Meta:
         model = Course
--- apps/klasses/serializers.py
+++ apps/klasses/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from apps.klasses.models import Klass
+from apps.students.serializers import StudentSerializer
 
 
 class KlassSerializer(serializers.ModelSerializer):
+    students = StudentSerializer(many = True, read_only = True)
+    
     class Meta:
         model = Klass
         fields = "__all__"
